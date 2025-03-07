32c09f0a860b87452843b7fd5fc8a3f0742c3069
Łukasz Gołębiewski <lukasz.golebiewski@pl.abb.com>
2025-03-07 14:59:51 +0100

    wip

--- commands.ps1
+++ commands.ps1
@@ -1,5 +1,8 @@
 #databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net --profile DEFAULT
 #databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net --profile DEV
+
+$databricksProfile = "DEFAULT"
+
 databricks auth profiles list
-databricks auth env --profile DEFAULT
+databricks auth env --profile $databricksProfile
 
