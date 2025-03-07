61399d6a4c99a69258f1b068da3c98b0a2b765ad
Łukasz Gołębiewski <lukasz.golebiewski@pl.abb.com>
2025-03-07 11:00:30 +0100

    wip

--- loging.ps1
+++ loging.ps1
@@ -2,3 +2,7 @@ databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net 
 databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net --profile DEV
 databricks auth profiles list
 databricks auth env --profile DEFAULT
+
+
+databricks bundle init --profile DEFAULT
+databricks bundle validate --profile DEFAULT
