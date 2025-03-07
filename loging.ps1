36c4d5628c9674bf66e9d54a8f5bec4de759efa4
Łukasz Gołębiewski <lukasz.golebiewski@pl.abb.com>
2025-03-07 10:27:22 +0100

    wip

---
+++ loging.ps1
@@ -0,0 +1,4 @@
+databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net --profile DEFAULT
+databricks auth login --host https://adb-152493134425274.14.azuredatabricks.net --profile DEV
+databricks auth profiles list
+databricks auth env --profile DEFAULT
